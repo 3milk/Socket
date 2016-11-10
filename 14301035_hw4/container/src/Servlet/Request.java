ed3daee92893e163753ca159571e967498217776
3milk <429407748@qq.com>
2016-11-10 11:05:29 +0800

    hw4_container+JSPengine

---
+++ 14301035_hw4/container/src/Servlet/Request.java
@@ -0,0 +1,600 @@
+package Servlet;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpUpgradeHandler;
+import javax.servlet.http.Part;
+
+public class Request implements HttpServletRequest{//ServletRequest {
+
+	private InputStream input;
+	private String uri;
+	private String method;
+	private Map<String,String> map;
+	boolean parseSuccess;
+
+
+
+	public Request(InputStream input) {
+		this.input = input;
+		map= new HashMap<String,String>();
+		parseSuccess = true;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	/**
+	 * requestString形式如下： GET /index.html HTTP/1.1 Host: localhost:8080
+	 * Connection: keep-alive Cache-Control: max-age=0 ...
+	 * 该函数目的就是为了获取/index.html字符串
+	 */
+	private String parseUri(String requestString) {
+		int index1, index2;
+		index1 = requestString.indexOf(' ');
+		if (index1 != -1) {
+			index2 = requestString.indexOf(' ', index1 + 1);
+			if (index2 > index1)
+				return requestString.substring(index1 + 1, index2);//Login?id=233
+		}
+		return null;
+	}
+
+	private String parseMethod(String requestString) {
+		int index1;
+		index1 = requestString.indexOf(' ');
+		if (index1 != -1) {
+			return requestString.substring(0, index1);
+		}
+		return null;
+	}
+	
+	// 从InputStream中读取request信息，并从request中获取uri值, method
+	public void parse() {
+		// Read a set of characters from the socket
+		StringBuffer request = new StringBuffer(2048);
+		int i;
+		byte[] buffer = new byte[2048];
+		try {
+			i = input.read(buffer);
+		} catch (IOException e) {
+			e.printStackTrace();
+			i = -1;
+		}
+		for (int j = 0; j < i; j++) {
+			request.append((char) buffer[j]);
+		}
+		//System.out.print(request.toString());
+		uri = parseUri(request.toString());
+		method = parseMethod(request.toString());
+		parseParameter(request.toString());
+	}
+
+	@Override
+	public String getMethod() {
+		// TODO Auto-generated method stub
+		return method;
+	}
+	
+	/**
+	 * 还要实现getParameter相关方法//////////////////////ATTENTION//////////////
+	 * getHeader 可选做
+	 * get&setCharacterEncoding 编码乱码问题(只涉及body内部，所以只对POST方法适用？)
+	 *              URLEncoding 针对GET方法传参
+	 * */
+	
+	/* implementation of the ServletRequest */
+	public Object getAttribute(String attribute) {
+		return null; 
+	}
+
+	public String getRealPath(String path) {
+		return null;
+	}
+
+	public RequestDispatcher getRequestDispatcher(String path) {
+		return null;
+	}
+
+	public boolean isSecure() {
+		return false;
+	}
+
+	public String getCharacterEncoding() {
+		return null;
+	}
+
+	public int getContentLength() {
+		return 0;
+	}
+
+	public String getContentType() {
+		return null;
+	}
+
+	
+	public ServletInputStream getInputStream() throws IOException {
+		return (ServletInputStream) this.input;
+	}
+
+	public Locale getLocale() {
+		return null;
+	}
+
+	public String getPostParaString(String requestString) {
+		int index1 = requestString.indexOf("\r\n\r\n");
+		String postString = null;
+		if(index1 != 1) {
+			postString = requestString.substring(index1+4, requestString.length());
+		}
+		return postString; //形如：name=233&password=23
+	}
+
+	public boolean parseParameter(String requestString) {
+		int decodeFailCount = 0;
+		
+		//判断get or post
+		//requestString: POST /Login/////.../n id=233&name=tom ...
+		//获取如：UserID=string&PWD=string&OrderConfirmation=string
+		String para = getPostParaString(requestString);//getUri();
+		byte[] bytes = para.getBytes();
+		
+        int pos = 0;//start;
+        int end = bytes.length;//start + len;
+        
+        while(pos < end) {
+            int nameStart = pos;
+            int nameEnd = -1;
+            int valueStart = -1;
+            int valueEnd = -1;
+
+            boolean parsingName = true;
+            boolean decodeName = false;
+            boolean decodeValue = false;
+            boolean parameterComplete = false;
+
+            do {
+            	switch(bytes[pos]) {
+                    case '=':
+                        if (parsingName) {
+                            // Name finished. Value starts from next character
+                            nameEnd = pos;
+                            parsingName = false;
+                            valueStart = ++pos;
+                        } else {
+                            // Equals character in value
+                            pos++;
+                        }
+                        break;
+                    case '&':
+                        if (parsingName) {
+                            // Name finished. No value.
+                            nameEnd = pos;
+                        } else {
+                            // Value finished
+                            valueEnd  = pos;
+                        }
+                        parameterComplete = true;
+                        pos++;
+                        break;
+                    case '%':
+                    case '+':
+                        // Decoding required
+                        if (parsingName) {
+                            decodeName = true;
+                        } else {
+                            decodeValue = true;
+                        }
+                        pos ++;
+                        break;
+                    default:
+                        pos ++;
+                        break;
+                }
+            } while (!parameterComplete && pos < end);
+
+            if (pos == end) {
+                if (nameEnd == -1) {
+                    nameEnd = pos;
+                } else if (valueStart > -1 && valueEnd == -1){
+                    valueEnd = pos;
+                }
+            }
+/////////////////////////////////////////////////
+
+            byte[] tmpName = new byte[nameEnd-nameStart];
+            byte[] tmpValue = null;
+            System.arraycopy(bytes, nameStart, tmpName, 0, nameEnd-nameStart);
+            if (valueStart >= 0) {
+                tmpValue = new byte[valueEnd-valueStart];
+            	System.arraycopy(bytes, valueStart, tmpValue, 0, valueEnd - valueStart);
+            } else {
+                tmpValue = new byte[1];            	
+            	System.arraycopy(bytes, 0, tmpValue, 0, 0);
+            }
+
+            
+            String pname;
+			String pvalue;
+
+			pname = new String(tmpName);
+			if (valueStart >= 0) {
+			    pvalue = new String(tmpValue);
+			} else {
+			    pvalue = "";
+			}
+			
+			try {
+				map.put(pname, pvalue);
+			} catch (IllegalStateException ise) {
+			    // Hitting limit stops processing further params but does
+			    // not cause request to fail.
+			    parseSuccess = false;
+			    break;
+			}
+        }
+		return parseSuccess;
+	}
+	
+	public String getParameter(String name) {
+		String value = null;
+		if(parseSuccess) {
+			for(String key : map.keySet()){ 
+				if(key.equals(name)) {
+	                value = map.get(key); 
+	                break;
+	            } 
+	        }
+		}
+		return value; //TODO TODO
+	}
+
+	public String[] getParameterValues(String parameter) {
+		return null; //TODO TODO
+	}
+
+	public String getProtocol() {
+		return null;
+	}
+
+	public BufferedReader getReader() throws IOException {
+		return null;
+	}
+
+	public String getRemoteAddr() {
+		return null;
+	}
+
+	public String getRemoteHost() {
+		return null;
+	}
+
+	public String getScheme() {
+		return null;
+	}
+
+	public String getServerName() {
+		return null;
+	}
+
+	public int getServerPort() {
+		return 0;
+	}
+
+	public void removeAttribute(String attribute) {
+	}
+
+	public void setAttribute(String key, Object value) {
+	}
+
+	public void setCharacterEncoding(String encoding)
+			throws UnsupportedEncodingException {
+	}
+
+	@Override
+	public AsyncContext getAsyncContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getContentLengthLong() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DispatcherType getDispatcherType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLocalAddr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLocalName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLocalPort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRemotePort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAsyncStarted() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAsyncSupported() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public AsyncContext startAsync() throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AsyncContext startAsync(ServletRequest arg0, ServletResponse arg1)
+			throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enumeration<String> getAttributeNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enumeration<Locale> getLocales() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String[]> getParameterMap() {
+		// TODO TODO TODO TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enumeration<String> getParameterNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean authenticate(HttpServletResponse arg0) throws IOException,
+			ServletException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String changeSessionId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAuthType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getContextPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cookie[] getCookies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getDateHeader(String arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getHeader(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enumeration<String> getHeaderNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enumeration<String> getHeaders(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getIntHeader(String arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Part getPart(String arg0) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Part> getParts() throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPathInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPathTranslated() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getQueryString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRemoteUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRequestURI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StringBuffer getRequestURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRequestedSessionId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpSession getSession() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpSession getSession(boolean arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Principal getUserPrincipal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isRequestedSessionIdFromCookie() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRequestedSessionIdFromURL() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRequestedSessionIdFromUrl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRequestedSessionIdValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isUserInRole(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void login(String arg0, String arg1) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logout() throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <T extends HttpUpgradeHandler> T upgrade(Class<T> arg0)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
